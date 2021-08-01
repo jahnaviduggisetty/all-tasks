15599c965cc3f37967b7fb767934bc73861c558f
jahavi <jahnaviduggisetty2gmail.com>
2021-08-01 19:16:36 +0530

    all

---
+++ Aabc.java
@@ -0,0 +1,11 @@
+package jahnavi;
+
+public class Aabc {
+
+	public static void main(String[] args) {
+		System.out.println("hiii");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Abcd.java
@@ -0,0 +1,9 @@
+package jahnavi;
+
+public class Abcd {
+	public static void main(String args[])
+	{
+	System.out.println("hiii");
+
+}
+}
